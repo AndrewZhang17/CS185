b1428e2ab75d6be5c2814d6ee592fdea2c3d4f3a
AndrewZhang17 <zhang.w.andrew@gmail.com>
2020-04-25 23:02:51 -0700

    Updates

---
+++ precache-manifest.90c60226aa305d43f2acb8034464f3a8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834dec983c4ba952fc2a33e3fa8ae779",
+    "url": "/CS185/index.html"
+  },
+  {
+    "revision": "6bcbbab8e261a1dbfaed",
+    "url": "/CS185/static/css/main.de27287e.chunk.css"
+  },
+  {
+    "revision": "705131adbff8e9226d31",
+    "url": "/CS185/static/js/2.071b47ae.chunk.js"
+  },
+  {
+    "revision": "04007fcd50e895bcbb2b21327be92c6e",
+    "url": "/CS185/static/js/2.071b47ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcbbab8e261a1dbfaed",
+    "url": "/CS185/static/js/main.8588fb7a.chunk.js"
+  },
+  {
+    "revision": "1c8e4edcdab0ef160322",
+    "url": "/CS185/static/js/runtime-main.328778fd.js"
+  },
+  {
+    "revision": "57c7854ad0274132f090d0a30c06a74c",
+    "url": "/CS185/static/media/Perry.57c7854a.png"
+  },
+  {
+    "revision": "738c1dd453aeeee53d5fdd02c6f9e657",
+    "url": "/CS185/static/media/beedance.738c1dd4.mov"
+  },
+  {
+    "revision": "5d14bbbeaf551ea51fbf93124e25dd60",
+    "url": "/CS185/static/media/caterpillar.5d14bbbe.jpg"
+  },
+  {
+    "revision": "0656a0cfc1b56d179387b8b567f534b2",
+    "url": "/CS185/static/media/dessert.0656a0cf.jpg"
+  },
+  {
+    "revision": "f3e572c2563d3b086cdab7875b0fb243",
+    "url": "/CS185/static/media/europa.f3e572c2.jpeg"
+  },
+  {
+    "revision": "f5b0e0b47c1a992be33796fde94ed70c",
+    "url": "/CS185/static/media/hiking.f5b0e0b4.jpg"
+  },
+  {
+    "revision": "94d15067df637ca52e0e7d22e3821e59",
+    "url": "/CS185/static/media/icon.94d15067.png"
+  },
+  {
+    "revision": "b30f51101988bd5fcd7b5f01b6393552",
+    "url": "/CS185/static/media/matcha.b30f5110.jpg"
+  },
+  {
+    "revision": "bc3e64997c15adee137abc620908e54e",
+    "url": "/CS185/static/media/msft.bc3e6499.jpg"
+  },
+  {
+    "revision": "dcbebe90365889e41921a7e4cb641efb",
+    "url": "/CS185/static/media/potatochip.dcbebe90.jpg"
+  },
+  {
+    "revision": "5ead1c0a1118ed1dceadc1662a6e6804",
+    "url": "/CS185/static/media/sbhacks.5ead1c0a.jpg"
+  },
+  {
+    "revision": "f2caca63312b94bb5b757c4207849616",
+    "url": "/CS185/static/media/sbhackswkshop.f2caca63.jpg"
+  },
+  {
+    "revision": "96150430e9dc3de9a667a25be8c8ca4f",
+    "url": "/CS185/static/media/tennis0.96150430.mp4"
+  },
+  {
+    "revision": "0978ccc794db01860e4a62bf358e4e61",
+    "url": "/CS185/static/media/tennis1.0978ccc7.mp4"
+  },
+  {
+    "revision": "8f5f3dc79730e7128f69d637c6a249e0",
+    "url": "/CS185/static/media/tennis2.8f5f3dc7.mp4"
+  }
+]);
