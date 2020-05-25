4634fd4b6f37ba4098e679b5228e2a201cef1255
AndrewZhang17 <zhang.w.andrew@gmail.com>
2020-05-24 17:44:30 -0700

    add firebase

--- myapp/src/Components/Movies.js
+++ myapp/src/Components/Movies.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 import { ids } from "../assets/movies.json"
 import Grid from './Grid';
 import Popup from "reactjs-popup";
+import config from '../config';
 const axios = require('axios');
+const firebase = require('firebase')
 
-class Movies extends Component {
+function Movies(props) {
+    const [movies, setMovies] = useState([]);
+    const [shouldRender, setShowRender] = useState(true);
 
-    constructor(props) {
-        super(props);
-  
-        this.state = {
-          movies : []
-        };
-    }
+    useEffect(() => {
+        if (!firebase.apps.length) {
+            firebase.initializeApp(config);
+        }
+        //get a reference to the database
+        let ref = firebase.database().ref('movies');
+        var loaded = false;
 
-    getMovies() {
+        ref.on('child_added', (childSnapshot, prevChildKey) => {
+            //this is your call back function
+            //state will be a JSON object after this
+            //set your apps state to contain this data however you like
+            const newChild = childSnapshot.val();
+            //i have previously declared a state variable like this: const [data, setData] = useState([]) so that I can make the below call
+            setMovies(curMovies => [...curMovies, newChild])
+            console.log(newChild.id);
+        })
+
+    }, [shouldRender])
+
+    const getMovies = () => {
         for(const id of ids) {
             axios({
                 method: 'get',
@@ -22,42 +38,43 @@ class Movies extends Component {
             })
             .then(
                 (response) => {
-                    this.setState(state => {
-                        return {movies: [...this.state.movies, response.data]};
-                    })
+                    firebase.database().ref('movies').child(response.data.imdbID).set({
+                        imdbID: response.data.imdbID,
+                        Title: response.data.Title,
+                        Poster: response.data.Poster,
+                        imdbRating: response.data.imdbRating,
+                        Director: response.data.Director,
+                        Released: response.data.Released,
+                        Plot: response.data.Plot
+                    });
                 }
             )
         }
     }
 
-    componentWillMount() {
-        this.getMovies()
-    }
-
-    render() {
-        const movies = this.state.movies.map(m =>
-            <Popup className="popup" trigger={<img className="movie-item" alt={m.Title} src={m.Poster}/>} modal closeOnDocumentClick lockScroll postion="center center">
-                <div className="movie-popup">
-                    <img className="movie-poster" alt={m.Title} src={m.Poster}/>                        
-                    <div className="movie-info">
-                        <h1>{m.Title}</h1>
-                        <div className="imdb-rating">IMDB Rating: {m.imdbRating}</div>
-                        <p><b>Director:</b> {m.Director}</p>
-                        <p><b>Released:</b> {m.Released}</p>
-                        <p><b>Synopsis:</b> {m.Plot}</p>
-                    </div>
+    const movs = movies.map(m =>
+        <Popup className="popup" trigger={<img className="movie-item" alt={m.Title} src={m.Poster}/>} modal closeOnDocumentClick lockScroll postion="center center">
+            <div className="movie-popup">
+                <img className="movie-poster" alt={m.Title} src={m.Poster}/>                        
+                <div className="movie-info">
+                    <h1>{m.Title}</h1>
+                    <div className="imdb-rating">IMDB Rating: {m.imdbRating}</div>
+                    <p><b>Director:</b> {m.Director}</p>
+                    <p><b>Released:</b> {m.Released}</p>
+                    <p><b>Synopsis:</b> {m.Plot}</p>
                 </div>
-            </Popup>
-        )
-        return (
-            <div>
-                <h1>
-                    Movies
-                </h1>
-                <Grid items={movies}/>
             </div>
-        );
-    }
+        </Popup>
+    )
+    return (
+        <div>
+            <h1>
+                Movies
+            </h1>
+            <Grid items={movs}/>
+        </div>
+    );
+
 }
 
 export default Movies;
