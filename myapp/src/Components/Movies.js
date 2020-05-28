ea93d9383d045fb02ab87e8ec62001e9b96fa23e
AndrewZhang17 <zhang.w.andrew@gmail.com>
2020-05-27 20:46:32 -0700

    pagination barely works with realtime database

--- myapp/src/Components/Movies.js
+++ myapp/src/Components/Movies.js
@@ -10,36 +10,48 @@ function Movies(props) {
     const [movies, setMovies] = useState([]);
     const [lists, setLists] = useState({});
     const [movieLists, setMovieLists] = useState({});
-    const [curList, setCurList] = useState("");
+    const [curList, setCurList] = useState("All");
     const [page, setPage] = useState(0);
     const [newId, setNewId] = useState("");
     const [newList, setNewList] = useState("");
     const [search, setSearch] = useState("");
+    const [count, setCount] = useState(0);
 
     const [shouldRender, setShowRender] = useState(true);
 
     useEffect(() => {
+        setCurList("All")
+
         if (!firebase.apps.length) {
             firebase.initializeApp(config);
         }
 
-        let ref = firebase.database().ref('movies');
+        let ref = firebase.database().ref('movies').orderByKey();
 
-        ref.on('child_added', (childSnapshot, prevChildKey) => {
-            const newChild = childSnapshot.val();
-            setMovies(curMovies => [...curMovies, newChild]);
-        });
-
-        ref.on('child_removed', (childSnapshot) => {
+        ref.on('child_removed', (childSnapshot, prevChildKey) => {
             const deletedChild = childSnapshot.val();
-            setMovies(curMovies => curMovies.filter(m => m.Title != deletedChild.Title));
+            setMovies(curMovies => curMovies.filter(m => m.imdbID != deletedChild.imdbID));
+            console.log(deletedChild.Title);
         });
+
+        ref.limitToFirst(8).once('value', (dataSnapshot) => {
+            const val = dataSnapshot.val();
+            if(val != null) {
+                setMovies(Object.values(val));
+                console.log(val);
+            }
+        });
+
+        
 
         let listRef = firebase.database().ref('lists');
 
         listRef.on('value', (dataSnapshot) => {
             const val = dataSnapshot.val();
             setLists(val);
+            if(val != null) {
+                setCount(Object.keys(val["All"]).length - 1);
+            }
         });
 
         let movieListsRef = firebase.database().ref('movieLists');
@@ -67,11 +79,18 @@ function Movies(props) {
                     Director: response.data.Director,
                     Released: response.data.Released,
                     Plot: response.data.Plot
+                }); 
+                firebase.database().ref('lists').child("All").child(response.data.imdbID).set(true);
+                firebase.database().ref('movieLists').child(response.data.imdbID).set({
+                    All: true
+                }).then(() => {
+                    setNewId("");
+                    setPage(0);
+                    setShowRender(cur => !cur);
                 });
             }
         )
-        setNewId("");
-        setPage(0);
+        
     }
 
     const deleteMovie = (id) => {
@@ -96,6 +115,7 @@ function Movies(props) {
 
         setNewList("");
         setPage(0);
+        setShowRender(cur => !cur);
     }
 
     const createPair = (id, list, target) => {
@@ -108,14 +128,39 @@ function Movies(props) {
         firebase.database().ref().update(updates);
     }
 
+    const setCurListHelper = (l) => {
+        setCurList(l);
+        let c = Object.keys(lists[l]).length - 1;
+        setCount(c);
+        setMovies([]);
+        let ref = firebase.database().ref('movies');
+        console.log(count);
+        for(var i = 1; i < Math.min(9, c+1); i++) {
+            ref.child(Object.keys(lists[l])[i]).once('value', (dataSnapshot) => {
+                const val = dataSnapshot.val();
+                setMovies(cur => [...cur, val]);
+            });
+        }
+    }
+    
+    const loadMore = () => {
+        let ref = firebase.database().ref('movies');
+        let curLen = movies.length;
+        for(var i = curLen + 1; i < Math.min(curLen + 9, Object.keys(lists[curList]).length); i++) {
+            ref.child(Object.keys(lists[curList])[i]).once('value', (dataSnapshot) => {
+                const val = dataSnapshot.val();
+                setMovies(cur => [...cur, val]);
+            });
+        }
+    }
+
     const getPage = () => {
         if (page === 0) {
             const movs = movies
-                .filter(m => curList === "" || lists[curList][m.imdbID] === true)
-                .filter(m => m.Title.toLowerCase().includes(search.toLowerCase()))
+                .filter(m => m != null && m.Title.toLowerCase().includes(search.toLowerCase()))
                 .map(m =>
                 <Popup className="popup" trigger={<img className="movie-item" alt={m.Title} src={m.Poster}/>} modal closeOnDocumentClick lockScroll postion="center center">
-                    <div className="movie-popup">
+                    {close => (<div className="movie-popup">
                         <img className="movie-poster" alt={m.Title} src={m.Poster}/>                        
                         <div className="movie-info">
                             <h1>{m.Title}</h1>
@@ -123,7 +168,7 @@ function Movies(props) {
                             <p><b>Director:</b> {m.Director}</p>
                             <p><b>Released:</b> {m.Released}</p>
                             <p><b>Synopsis:</b> {m.Plot}</p>
-                            <button className="delete-movie" onClick={() => deleteMovie(m.imdbID)}>
+                            <button className="delete-movie" onClick={() => {deleteMovie(m.imdbID); close();}}>
                                 Delete
                             </button>
                             <select className="movie-btn" onChange={e => createPair(m.imdbID, e.target.value, e.target)}>
@@ -138,7 +183,7 @@ function Movies(props) {
                                 }
                             </select>
                         </div>                        
-                    </div>
+                    </div>)}
                 </Popup>
             )
             return (
@@ -147,10 +192,10 @@ function Movies(props) {
                         Movies
                     </h1>
                     <div className="movies-menu">
-                        <select className="movie-btn" onChange={e => setCurList(e.target.value)}>
-                            <option value="">All</option>
-                            {Object.keys(lists).map(l => (
-                                <option value={l}>{l}</option>
+                        <select className="movie-btn" onChange={e => setCurListHelper(e.target.value)}>
+                            <option value="All">All</option>
+                            {lists != null && Object.keys(lists).map(l => (
+                                l != "All" && <option value={l}>{l}</option>
                             ))}
                         </select>
                         <button className="movie-btn" onClick={() => setPage(1)}>
@@ -162,6 +207,9 @@ function Movies(props) {
                         <input className="movie-search" type="text" placeholder="Search.." value={search} onChange={e => setSearch(e.target.value)}/>
                     </div>
                     <Grid items={movs}/>
+                    {lists != null && lists[curList] != null && count > movies.length && <button className="load-more" onClick={() => loadMore()}>
+                        Load More
+                    </button>}
                 </div>
             );
         }
@@ -215,7 +263,6 @@ function Movies(props) {
         }
     }
 
-    console.log(lists);
     return getPage();
     
 
